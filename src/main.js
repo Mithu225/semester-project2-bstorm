87b7d8f4388f9def9976c187f0028181fb16b965
Mithu225 <thu@opendigital.no>
2024-11-16 13:48:52 +0100

    css fw, html, toogle login and register form

--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="/login">login</a> |
-    <a href="/profile">profile</a>
-  </div>
-`;
+// document.querySelector("#app").innerHTML = `
+//   <div>
+//     <a href="/login">login</a> |
+//     <a href="/profile">profile</a>
+//   </div>
+// `;
+
+const loginButtonElm = document.querySelector("#login-button");
+const loginFormElm = document.querySelector("#login-form");
+
+loginButtonElm.addEventListener("click", () => {
+  loginFormElm.classList.toggle("hidden");
+});
+
+const registerButtonElm = document.querySelector("#register-button");
+const registerFormElm = document.querySelector("#register-form");
+
+registerButtonElm.addEventListener("click", () => {
+  loginFormElm.classList.toggle("hidden");
+  registerFormElm.classList.toggle("hidden");
+});
+
+const closeLoginButtonElm = document.querySelector("#close-login-button");
+
+closeLoginButtonElm.addEventListener("click", () => {
+  loginFormElm.classList.add("hidden");
+  registerFormElm.classList.add("hidden");
+});
+const closeRegisterButtonElm = document.querySelector("#close-register-button");
+closeRegisterButtonElm.addEventListener("click", () => {
+  loginFormElm.classList.add("hidden");
+  registerFormElm.classList.add("hidden");
+});
